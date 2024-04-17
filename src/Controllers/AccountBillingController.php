e60b5480740b22886d9219a550054ab9e3c093ad
Jae Muzzin <jae@sonarlinux.muzzin.home.kg>
2024-04-17 11:25:19 -0400

    Get Services

--- src/Controllers/AccountBillingController.php
+++ src/Controllers/AccountBillingController.php
@@ -42,6 +42,11 @@ class AccountBillingController
         return $this->httpHelper->get("accounts/" . intval($accountID) . "/services", $page);
     }
 
+    public function getService($serviceID, $page = 1)
+    {
+        return $this->httpHelper->get("services/" . intval(serviceID), $page);
+    }
+
     /**
      * Returns an invoice as a base64 encoded string (see https://sonar.software/apidoc/index.html#api-Account_Invoices-GetInvoicePdf)
      * @param $accountID
